311637bd4d3460488cfb76430f12151d2ee1947c
WERGHI Ahmed <ahmed.werghi@oscaro.com>
2025-06-28 00:48:40 +0200

    lifespan of the app

---
+++ backend/app/main.py
@@ -0,0 +1,45 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from starlette import status
+
+
+from app.api.v1.endpoints import book
+from app.core.config import settings
+from app.core.log import logger
+from app.db.session import engine
+
+
+@asynccontextmanager
+async def lifespan (app : FastAPI):
+
+    logger.info("Starting application... ")
+
+    yield
+
+    logger.info("shutting down application ...")
+
+
+
+app  = FastAPI(
+    lifespan= lifespan,
+    title= settings.PROJECT_NAME,
+    description= settings.PROJECT_DESCRIPTION,
+    version= settings.PROJECT_VERSION,
+    environment = settings.PROJECT_ENVIRONMENT,
+    prefix= settings.API_VERSION
+)
+
+app.include_router(book.router)
+
+
+
+@app.get("/",status_code=status.HTTP_200_OK)
+async def root():
+    logger.info("Application is accessed")
+    return {"message" : "welcome to BookNest, an app to handle your library! "}
+
+
+
+
+
