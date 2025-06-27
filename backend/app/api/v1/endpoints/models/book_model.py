4ba2577bd30a5c007263aaa85dd798e249d8177a
WERGHI Ahmed <ahmed.werghi@oscaro.com>
2025-06-28 00:48:05 +0200

    add endpoint response and request models

---
+++ backend/app/api/v1/endpoints/models/book_model.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class LanguageModel (str,Enum):
+
+    EN = "en"
+    AR = "ar"
+    FR = "fr"
+
+
+
+class BookModel(BaseModel):
+
+    id : int = Field(gt=0)
+    title : str
+    subtitle : str
+    isbn_10 :  str = Field(min_length=10,max_length=10)
+    isbn_13 : str = Field(min_length=13,max_length=13)
+    language : LanguageModel
+    edition : str
+    publication_date : str
